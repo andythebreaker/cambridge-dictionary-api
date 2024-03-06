ef898c28dba504ab610a652852b305fb2c0c162c
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2024-03-06 22:56:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const http = require("http");
-const data = require("./data");
+import http from "http";
+import data from "./data.js";
+// Import the `config` function from the `dotenv` package
+import { config } from 'dotenv';
+// Load environment variables from the .env file
+config();
 const port = process.env.PORT || 3000;
 const server = http.createServer(data);
 
-console.log("Server running at http://localhost:3000");
+console.log(`Server running at http://localhost:${port}`);
 console.log("try /api/dictionary/english/hello");
 console.log("or use / to test the api with UI");
 
